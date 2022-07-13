a049431083225bd2ede718193a49c4b7c47e6a2b
Sandun Abeywickrama <sandunfrozenmkw@gmail.com>
2022-07-13 14:15:50 +0530

    refactor

--- Account Balance Viewer/src/app/home/home.component.ts
+++ Account Balance Viewer/src/app/home/home.component.ts
@@ -12,13 +12,13 @@ export class HomeComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getPublicContent().subscribe(
-      data => {
-        this.content = data;
-      },
-      err => {
-        this.content = JSON.parse(err.error).message;
-      }
-    );
+    // this.userService.getPublicContent().subscribe(
+    //   data => {
+    //     this.content = data;
+    //   },
+    //   err => {
+    //     this.content = JSON.parse(err.error).message;
+    //   }
+    // );
   }
 }
