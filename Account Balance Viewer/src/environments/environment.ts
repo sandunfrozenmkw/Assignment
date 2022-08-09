d5feb10d535906f3fceaddd98989b72dada35592
sandunfrozenmkw@gmail.com <SandunProject@123>
2022-08-09 20:21:19 +0530

    refactor

--- Account Balance Viewer/src/environments/environment.ts
+++ Account Balance Viewer/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  // baseApiUrl: 'http://localhost:13588/',
-  baseApiUrl: 'https://accountbalanceviewerapinew.azurewebsites.net/',
+  baseApiUrl: 'http://localhost:13588/',
+  // baseApiUrl: 'https://accountbalanceviewerapinew.azurewebsites.net/',
   frontEndUrl: 'https://red-forest-0c88a6210.1.azurestaticapps.net/',
   //frontEndUrl: 'http://localhost:4200/',
   production: false,
