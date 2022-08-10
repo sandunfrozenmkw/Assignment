62ab50e55203c71f7b220923354c9b5b27af38ca
sandunfrozenmkw@gmail.com <SandunProject@123>
2022-08-10 06:47:36 +0530

    unit tests

---
+++ AccountBalanceViewerAPITest/AccountBalanceTest.cs
@@ -0,0 +1,12 @@
+namespace AccountBalanceViewerAPITest
+{
+    [TestClass]
+    public class AccountBalanceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
